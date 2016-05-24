83c113c8be4f87b7dfdd7b220acd12c9c7bf7378
Skip Christian <skip.xtian@gmail.com>
2016-05-24 22:46:03 +1000

    Added in a zero offset for the arm angle, fixed so positive number move both arms forward

--- Armed.ino
+++ Armed.ino
@@ -1,17 +1,28 @@
 //testMode Branch
-
-#include "as5048a.h"
+#include <SPI.h>
+//#include "as5048a.h"
 #include "math.h"
 #include "pins.h"
 
+SPISettings settings;
+#define _cs 53
+word foreArmPosition, mainArmPosition;
+//bool errorFlag;
+const int AS5048A_CLEAR_ERROR_FLAG              = 0x0001;
+const int AS5048A_PROGRAMMING_CONTROL           = 0x0003;
+const int AS5048A_OTP_REGISTER_ZERO_POS_HIGH    = 0x0016;
+const int AS5048A_OTP_REGISTER_ZERO_POS_LOW     = 0x0017;
+const int AS5048A_DIAG_AGC                      = 0x3FFD;
+const int AS5048A_MAGNITUDE                     = 0x3FFE;
+const int AS5048A_ANGLE                         = 0x3FFF;
 //FOREARM_DIR HIGH = Back, LOW = Forward
 //MAINARM_DIR HIGH = Forward, LOW = Back
 /* 6000 = 90 degrees, 1 step = 0.015 degrees
- * Vertical for ForeArm = 5400 steps from back limit switch (home position is -1075)
- * gotoHome() Moves ForeArm to back limit switch then forward 4325 steps, then moves mainArm back to meet ForeArm. MainArm is vertical.
- */
+   Vertical for ForeArm = 5400 steps from back limit switch (home position is -1075)
+   gotoHome() Moves ForeArm to back limit switch then forward 4325 steps, then moves mainArm back to meet ForeArm. MainArm is vertical.
+*/
 
-AS5048A angleSensor(53);
+//AS5048A angleSensor(53);
 
 #define BACKLIGHT_PIN     13
 
@@ -22,8 +33,8 @@ int foreArmSteps, mainArmSteps, rotationalSteps;
 int foreArmTarget, mainArmTarget, rotationalTarget;
 int foreArmPos, mainArmPos, rotationalPos;
 
-int halfStepDurationMS = 1200;                          // Calculated value to set 1/2 step pulse timer (mS x 2)   
-                                            
+int halfStepDurationMS = 1200;                          // Calculated value to set 1/2 step pulse timer (mS x 2)
+
 long stepsLeft = 0;                                  // Number of Steps
 boolean running = false;                             // Arm moving?
 
@@ -31,11 +42,11 @@ long timeNow = 0;
 long cycleTime = 1000;
 //int xPos, yPos, zPos;
 
-void setup() 
-{  
+void setup()
+{
   initSteppers();
   //initEndstops();
-  angleSensor.init();
+  AS5048Ainit();
 
   Serial.begin(115200);
   Serial.setTimeout(10);
@@ -43,124 +54,128 @@ void setup()
   Serial.println("Enter m=MainArm,f=ForeArm,s=Step speed, e=enable/disable");
 
 
-  setTimer1(halfStepDurationMS); 
-  
- 
+  setTimer1(halfStepDurationMS);
+
+
 
   //gotoHome();
   //test90();
   //foreArmPos = -1075;
   //mainArmPos = 0;
-  
+
 }
 
-void drawSquare() 
+void drawSquare()
 {
   goDirectlyTo(0, 100, 0);
   delay(1000);
-  for(byte i = 100; i <= 200; i++) {
+  for (byte i = 100; i <= 200; i++) {
     //delay(10);
-    while(running == true) {
+    while (running == true) {
       delay(1);
     } //wait for move to complete
     goDirectlyTo(0, i, 0);
   }
-  for(byte i = 0; i <= 100; i++) {
-    while(running == true) {
+  for (byte i = 0; i <= 100; i++) {
+    while (running == true) {
       delay(1);
     } //wait for move to complete
     goDirectlyTo(0, 200, i);
   }
-  for(byte i = 200; i >= 100; i--) {
-    while(running == true) {
+  for (byte i = 200; i >= 100; i--) {
+    while (running == true) {
       delay(1);
     } //wait for move to complete
     goDirectlyTo(0, i, 100);
   }
-  for(byte i = 100; i >= 0; i--) {
-    while(running == true) {
+  for (byte i = 100; i >= 0; i--) {
+    while (running == true) {
       delay(1);
     } //wait for move to complete
     goDirectlyTo(0, 100, i);
   }
-  
+
 }
 //long timeLast = 0;
 
 
-void loop () 
+void loop ()
 {
   static boolean lastRunState = false;
-  if(running != lastRunState){
-    Serial.println("Move Complete");   
-    if(lastRunState == true){
-    steppersEnabled(false);
+  if (running != lastRunState) {
+    Serial.println("Move Complete");
+    if (lastRunState == true) {
+      steppersEnabled(false);
     }
     lastRunState = running;
   }
   //drawSquare();
-  if(Serial.available()){
+  if (Serial.available()) {
     char cmd = Serial.read();
     int amount = Serial.parseInt();
-    
+
     Serial.print(amount);
     Serial.println(cmd);
-    if(cmd == 'm'){
-        
-        //digitalWrite(MAINARM_ENABLE_PIN    , LOW);
-      boolean targetDir = (amount >= 0)? LOW:HIGH;    
-      digitalWrite(MAINARM_DIR_PIN,targetDir);
+    if (cmd == 'm') {
+
+      //digitalWrite(MAINARM_ENABLE_PIN    , LOW);
+      boolean targetDir = (amount >= 0) ? LOW : HIGH;
+      digitalWrite(MAINARM_DIR_PIN, targetDir);
       mainArmSteps = amount;
       mainArmSteps = abs(mainArmSteps);
       Serial.print("Moving steps: "); Serial.print(mainArmSteps);
       running = true;
       steppersEnabled(true);
       ENABLE_STEPPER_DRIVER_INTERRUPT();
-      //while(running==true){}     
+      //while(running==true){}
       Serial.print("DONE with halfStepDurationMS set to "); Serial.print(halfStepDurationMS);
       //digitalWrite(MAINARM_ENABLE_PIN    , HIGH);
-    } else if(cmd == 'f'){
+    } else if (cmd == 'f') {
       //digitalWrite(FOREARM_ENABLE_PIN    , LOW);
-      boolean targetDir = (amount >= 0)? LOW:HIGH;    
-      digitalWrite(FOREARM_DIR_PIN,targetDir);
+      boolean targetDir = (amount >= 0) ? HIGH : LOW;
+      digitalWrite(FOREARM_DIR_PIN, targetDir);
       foreArmSteps = amount;
       foreArmSteps = abs(foreArmSteps);
       Serial.print("Moving steps: "); Serial.print(foreArmSteps);
       running = true;
       steppersEnabled(true);
       ENABLE_STEPPER_DRIVER_INTERRUPT();
-      //while(running==true){}     
+      //while(running==true){}
       Serial.print("DONE with halfStepDurationMS set to "); Serial.print(halfStepDurationMS);
       //digitalWrite(FOREARM_ENABLE_PIN    , HIGH);
-    } else if(cmd == 's'){
+    } else if (cmd == 's') {
       halfStepDurationMS = amount;
       setTimer1(halfStepDurationMS);
       Serial.print("halfStepDurationMS set to "); Serial.println(halfStepDurationMS);
-    } else if(cmd == 'e'){
+    } else if (cmd == 'e') {
       steppersEnabled(false);
     }
-    
+
     /*
-    int getForeArmTarget = Serial.parseInt();
-    int getMainArmTarget = Serial.parseInt();
-    int getRotationalTarget = Serial.parseInt();
-    
-    Serial.print("Converting from Cartesian : ");
-    Serial.print(getForeArmTarget);  Serial.print(" : ");
-    Serial.print(getMainArmTarget);  Serial.print(" : ");
-    Serial.println(getRotationalTarget);  
-    Serial.println("Moving Steppers To...");
-    
-    goDirectlyTo(getForeArmTarget,getMainArmTarget,getRotationalTarget);   
-    */ 
+      int getForeArmTarget = Serial.parseInt();
+      int getMainArmTarget = Serial.parseInt();
+      int getRotationalTarget = Serial.parseInt();
+
+      Serial.print("Converting from Cartesian : ");
+      Serial.print(getForeArmTarget);  Serial.print(" : ");
+      Serial.print(getMainArmTarget);  Serial.print(" : ");
+      Serial.println(getRotationalTarget);
+      Serial.println("Moving Steppers To...");
+
+      goDirectlyTo(getForeArmTarget,getMainArmTarget,getRotationalTarget);
+    */
   }
-  if(millis() - timeNow > cycleTime){
-      angleSensor.getRawRotation();
-      
-      timeNow = millis();
-    }
+  if (millis() - timeNow > cycleTime) {
+    getRotation();
+    /*word foreArmAngle, mainArmAngle;
+    getArmAngles(&foreArmAngle, &mainArmAngle);
+    Serial.print(foreArmAngle);
+    Serial.print(" : ");
+    Serial.println(mainArmAngle);*/
+    timeNow = millis();
+  }
   /*static bool travel = 1;
-  if(!running) {
+    if(!running) {
     if(travel) {
       driveStepper(1000);
       travel = !travel;
@@ -169,68 +184,68 @@ void loop ()
       driveStepper(-1000);
       travel = !travel;
     }
-  } */
+    } */
 
 
 }
 
-void test90() 
+void test90()
 {
   int dTime = 450;
   digitalWrite(MAINARM_DIR_PIN    , HIGH);
   for (unsigned int i = 6000; i != 0; i--) {
     digitalWrite(MAINARM_STEP_PIN    , HIGH);
-    delayMicroseconds(dTime); 
+    delayMicroseconds(dTime);
     digitalWrite(MAINARM_STEP_PIN    , LOW);
     delayMicroseconds(dTime);
   }
 }
 
-void gotoHome() 
+void gotoHome()
 {
-  int dTime = 450;  
+  int dTime = 450;
   digitalWrite(FOREARM_DIR_PIN    , HIGH);
-  while(digitalRead(FOREARM_MIN_PIN)){   //Travel all the way back till endStop
+  while (digitalRead(FOREARM_MIN_PIN)) { //Travel all the way back till endStop
     digitalWrite(FOREARM_STEP_PIN    , HIGH);
-    delayMicroseconds(dTime); 
+    delayMicroseconds(dTime);
     digitalWrite(FOREARM_STEP_PIN    , LOW);
     delayMicroseconds(dTime);
   }
 
   foreArmSteps = 0;
-  mainArmSteps = 0;  
+  mainArmSteps = 0;
 
   delay(1000);
 
   digitalWrite(FOREARM_DIR_PIN    , LOW);
-  while(foreArmSteps <= 4325) {                          //Travel all the way up till endStop 
+  while (foreArmSteps <= 4325) {                         //Travel all the way up till endStop
     digitalWrite(FOREARM_STEP_PIN    , HIGH);
-    delayMicroseconds(dTime); 
+    delayMicroseconds(dTime);
     digitalWrite(FOREARM_STEP_PIN    , LOW);
     delayMicroseconds(dTime);
     foreArmSteps++;
     if (!digitalRead(FOREARM_MAX_PIN)) {                  // If we hit the main Arm, move it into position
 
       Serial.println(foreArmSteps);
-      
+
       unsigned int diff = 4325 - foreArmSteps;
       digitalWrite(MAINARM_DIR_PIN    , HIGH);
       for (unsigned int i = diff; i != 0; i--) {
         digitalWrite(MAINARM_STEP_PIN    , HIGH);
-        delayMicroseconds(dTime); 
+        delayMicroseconds(dTime);
         digitalWrite(MAINARM_STEP_PIN    , LOW);
         delayMicroseconds(dTime);
       }
     }
   }
-  
+
 
   digitalWrite(MAINARM_DIR_PIN    , LOW);
-  while(digitalRead(FOREARM_MAX_PIN)) {  
-      digitalWrite(MAINARM_STEP_PIN    , HIGH);
-      delayMicroseconds(dTime); 
-      digitalWrite(MAINARM_STEP_PIN    , LOW);
-      delayMicroseconds(dTime);
+  while (digitalRead(FOREARM_MAX_PIN)) {
+    digitalWrite(MAINARM_STEP_PIN    , HIGH);
+    delayMicroseconds(dTime);
+    digitalWrite(MAINARM_STEP_PIN    , LOW);
+    delayMicroseconds(dTime);
   }
   delay(1000);
   foreArmPos = -1075;
--- as5048a.ino
+++ as5048a.ino
@@ -1,32 +1,10 @@
-#include "Arduino.h"
-
-#include "as5048a.h"
-
-//#define AS5048A_DEBUG
-
-const int AS5048A_CLEAR_ERROR_FLAG              = 0x0001;
-const int AS5048A_PROGRAMMING_CONTROL           = 0x0003;
-const int AS5048A_OTP_REGISTER_ZERO_POS_HIGH    = 0x0016;
-const int AS5048A_OTP_REGISTER_ZERO_POS_LOW     = 0x0017;
-const int AS5048A_DIAG_AGC                      = 0x3FFD;
-const int AS5048A_MAGNITUDE                     = 0x3FFE;
-const int AS5048A_ANGLE                         = 0x3FFF;
-
-/**
- * Constructor
- */
-AS5048A::AS5048A(byte arg_cs){
-  _cs = arg_cs;
-  errorFlag = false;
-  position = 0;
-}
-
 
 /**
  * Initialiser
  * Sets up the SPI interface
  */
-void AS5048A::init(){
+void AS5048Ainit(){
+  setZeroPosition(13750, 8530);
   // 1MHz clock (AMS should be able to accept up to 10MHz)
   settings = SPISettings(1000000, MSBFIRST, SPI_MODE1);
   
@@ -41,14 +19,14 @@ void AS5048A::init(){
  * Closes the SPI connection
  * SPI has an internal SPI-device counter, for each init()-call the close() function must be called exactly 1 time
  */
-void AS5048A::close(){
+void AS5048Aclose(){
   SPI.end();
 }
 
 /**
  * Utility function used to calculate even parity of word
  */
-byte AS5048A::spiCalcEvenParity(word value){
+byte spiCalcEvenParity(word value){
   byte cnt = 0;
   byte i;
 
@@ -63,98 +41,9 @@ byte AS5048A::spiCalcEvenParity(word value){
   return cnt & 0x1;
 }
 
-
-
-/**
- * Get the rotation of the sensor relative to the zero position.
- *
- * @return {int} between -2^13 and 2^13
- */
-int AS5048A::getRotation(){
-  word data;
-  int rotation;
-
-  data = AS5048A::getRawRotation();
-  rotation = (int)data - (int)position;
-  if(rotation > 8191) rotation = -((0x3FFF)-rotation); //more than -180
-  //if(rotation < -0x1FFF) rotation = rotation+0x3FFF;
-
-  return rotation;
-}
-
-/**
- * Returns the raw angle directly from the sensor
- */
-word AS5048A::getRawRotation(){
-  return AS5048A::read(AS5048A_ANGLE);
-}
-
-/**
- * returns the value of the state register
- * @return 16 bit word containing flags
- */
-word AS5048A::getState(){
-  return AS5048A::read(AS5048A_DIAG_AGC);
-}
-
-/**
- * Print the diagnostic register of the sensor
- */
-void AS5048A::printState(){
-  word data;
-
-  data = AS5048A::getState();
-  if(AS5048A::error()){
-    Serial.print("Error bit was set!");
-  }
-  Serial.println(data, BIN);
-}
-
-/**
- * Returns the value used for Automatic Gain Control (Part of diagnostic
- * register)
- */
-byte AS5048A::getGain(){
-  word data = AS5048A::getState();
-  return (byte) data & 0xFF;
-}
-
-/*
- * Get and clear the error register by reading it
- */
-word AS5048A::getErrors(){
-  return AS5048A::read(AS5048A_CLEAR_ERROR_FLAG);
-}
-
-/*
- * Set the zero position
- */
-void AS5048A::setZeroPosition(word arg_position){
-  position = arg_position % 0x3FFF;
-}
-
-/*
- * Returns the current zero position
- */
-word AS5048A::getZeroPosition(){
-  return position;
-}
-
-/*
- * Check if an error has been encountered.
- */
-bool AS5048A::error(){
-  return errorFlag;
-}
-
-/*
- * Read a register from the sensor
- * Takes the address of the register as a 16 bit word
- * Returns the value of the register
- */
-word AS5048A::read(word registerAddress){
+void getArmAngles(word *_foreArmAngle, word *_mainArmAngle){
   word command = 0b0100000000000000; // PAR=0 R/W=R
-  command = command | registerAddress;
+  command = command | AS5048A_ANGLE;
 
   //Add a parity bit on the the MSB
   command |= ((word)spiCalcEvenParity(command)<<15);
@@ -163,17 +52,9 @@ word AS5048A::read(word registerAddress){
   byte right_byte = command & 0xFF;
   byte left_byte = ( command >> 8 ) & 0xFF;
 
-#ifdef AS5048A_DEBUG
-  Serial.print("Read (0x");
-  Serial.print(registerAddress, HEX);
-  Serial.print(") with command: 0b");
-  Serial.println(command, BIN);
-#endif
-
   //SPI - begin transaction
   SPI.beginTransaction(settings);
 
-  
   //Send the command
     digitalWrite(_cs, LOW);
     SPI.transfer(left_byte);
@@ -181,8 +62,6 @@ word AS5048A::read(word registerAddress){
     SPI.transfer(left_byte);
     SPI.transfer(right_byte);
     digitalWrite(_cs,HIGH);
-    //Serial.println((( ( read_left_byte & 0xFF ) << 8 ) | ( read_right_byte & 0xFF )) & ~0xC000);
-    //delay(200);
   
   //Now read the response
   digitalWrite(_cs, LOW);
@@ -195,29 +74,139 @@ word AS5048A::read(word registerAddress){
   //SPI - end transaction
   SPI.endTransaction();
 
-#ifdef AS5048A_DEBUG
-  Serial.print("Read returned: ");
-  Serial.print(left_byte, BIN);
-  Serial.print(" ");
-  Serial.println(right_byte, BIN);
-#endif
+  *_foreArmAngle = (( ( a1_left_byte & 0xFF ) << 8 ) | ( a1_right_byte & 0xFF )) & ~0xC000;
+  //Serial.print(" : ");
+  *_mainArmAngle = (( ( a2_left_byte & 0xFF ) << 8 ) | ( a2_right_byte & 0xFF )) & ~0xC000;
+  //Return the data, stripping the parity and error bits
+  //return (( ( left_byte & 0xFF ) << 8 ) | ( right_byte & 0xFF )) & ~0xC000;
+}
 
-  //Check if the error bit is set
-  if (a1_left_byte & 0x40) {
-    //Serial.println("Setting A1 Error bit");
-    errorFlag = true;    
+/**
+ * Get the rotation of the sensor relative to the zero position.
+ *
+ * @return {int} between -2^13 and 2^13
+ */ /*word *_foreArmAngle, word *_mainArmAngle*/
+void getRotation(){
+  //word data;
+  //int rotation;
+  word foreArmAngle, mainArmAngle;
+  getArmAngles(&foreArmAngle, &mainArmAngle);
+  int fArmRotation = (int)foreArmAngle - (int)foreArmPosition;
+  if(fArmRotation > 8191) fArmRotation = -((0x3FFF)-fArmRotation); //more than -180
+  //if(rotation < -0x1FFF) rotation = rotation+0x3FFF;
+  int mArmRotation = (int)mainArmAngle - (int)mainArmPosition;
+  if(mArmRotation > 8191) mArmRotation = -((0x3FFF)-mArmRotation); //more than -180
+  //return rotation;
+  Serial.print(fArmRotation*-1);
+  Serial.print(" : ");
+  Serial.println(mArmRotation);
+}
+/*
+ * Set the zero position
+ */
+void setZeroPosition(word arg_fArmposition, word arg_mArmposition){
+  foreArmPosition = arg_fArmposition % 0x3FFF;
+  mainArmPosition = arg_mArmposition % 0x3FFF;
+}
+
+/*
+ * Returns the raw angle directly from the sensor
+ */
+/*void getRawRotation(){
+  AS5048Aread(AS5048A_ANGLE);
+}*
+
+/**
+ * returns the value of the state register
+ * @return 16 bit word containing flags
+ */
+/*word AS5048AgetState(){
+  return AS5048Aread(AS5048A_DIAG_AGC);
+}
+
+/**
+ * Print the diagnostic register of the sensor
+ */
+/*void AS5048AprintState(){
+  word data;
+
+  data = AS5048AgetState();
+  if(AS5048Aerror()){
+    Serial.print("Error bit was set!");
   }
-  else {
-    errorFlag = false;
-  }
-  //Check if the error bit is set
-  if (a2_left_byte & 0x40) {
-    //Serial.println("Setting A2 Error bit");
-    errorFlag = true;    
-  }
-  else {
-    errorFlag = false;
-  }
+  Serial.println(data, BIN);
+}
+
+/**
+ * Returns the value used for Automatic Gain Control (Part of diagnostic
+ * register)
+ */
+/*byte AS5048AgetGain(){
+  word data = AS5048AgetState();
+  return (byte) data & 0xFF;
+}
+
+/*
+ * Get and clear the error register by reading it
+ */
+/*word AS5048AgetErrors(){
+  return AS5048Aread(AS5048A_CLEAR_ERROR_FLAG);
+}
+
+
+
+/*
+ * Returns the current zero position
+ */
+/*word AS5048AgetZeroPosition(){
+  return position;
+}
+
+/*
+ * Check if an error has been encountered.
+ */
+/*bool AS5048Aerror(){
+  return errorFlag;
+}
+
+/*
+ * Read a register from the sensor
+ * Takes the address of the register as a 16 bit word
+ * Returns the value of the register
+ */
+word AS5048Aread(word registerAddress){
+  word command = 0b0100000000000000; // PAR=0 R/W=R
+  command = command | registerAddress;
+
+  //Add a parity bit on the the MSB
+  command |= ((word)spiCalcEvenParity(command)<<15);
+
+  //Split the command into two bytes
+  byte right_byte = command & 0xFF;
+  byte left_byte = ( command >> 8 ) & 0xFF;
+
+  //SPI - begin transaction
+  SPI.beginTransaction(settings);
+
+  
+  //Send the command
+    digitalWrite(_cs, LOW);
+    SPI.transfer(left_byte);
+    SPI.transfer(right_byte);
+    SPI.transfer(left_byte);
+    SPI.transfer(right_byte);
+    digitalWrite(_cs,HIGH);
+  
+  //Now read the response
+  digitalWrite(_cs, LOW);
+  byte a1_left_byte = SPI.transfer(0x00);
+  byte a1_right_byte = SPI.transfer(0x00);
+  byte a2_left_byte = SPI.transfer(0x00);
+  byte a2_right_byte = SPI.transfer(0x00);
+  digitalWrite(_cs, HIGH);
+
+  //SPI - end transaction
+  SPI.endTransaction();
 
   Serial.print((( ( a1_left_byte & 0xFF ) << 8 ) | ( a1_right_byte & 0xFF )) & ~0xC000);
   Serial.print(" : ");
@@ -234,7 +223,7 @@ word AS5048A::read(word registerAddress){
  * Returns the value of the register after the write has been performed. This
  * is read back from the sensor to ensure a sucessful write.
  */
-word AS5048A::write(word registerAddress, word data) {
+/*word AS5048Awrite(word registerAddress, word data) {
 
   word command = 0b0000000000000000; // PAR=0 R/W=W
   command |= registerAddress;
@@ -293,4 +282,4 @@ word AS5048A::write(word registerAddress, word data) {
   //Return the data, stripping the parity and error bits
   return (( ( left_byte & 0xFF ) << 8 ) | ( right_byte & 0xFF )) & ~0xC000;
 }
-
+*/
